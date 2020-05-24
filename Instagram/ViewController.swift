92c454d6a2f5cae62d972af3d4c302863d313965
Chase Chipres <chasechipres@gmail.com>
2020-05-24 13:30:20 -0700

    Initial Commit

---
+++ Instagram/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Instagram
+//
+//  Created by Chase Chipres on 5/24/20.
+//  Copyright © 2020 Chase Chipres. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
